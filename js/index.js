2b322e993cbbf615b3a03b6a3f91fce390bc65a5
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-05-03 14:05:17 +0300

    small fix, page border style

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import ryuMoveset from "./ryuMoveset.js";
+import ryuMoveset from "../data/ryuMoveset.js";
 
 const butWrap = document.querySelector(".moveset-btn-wrap");
 const buttons = document.querySelectorAll(".moveset-btn");
@@ -6,9 +6,10 @@ const imgAction = document.querySelector("#img-action");
 
 function showMoveset({ target }) {
   const action = target.dataset.action;
-  buttons.forEach((el) => el.classList.remove("active-btn"));
 
   if (action) {
+    buttons.forEach((el) => el.classList.remove("active-btn"));
+
     target.classList.add("active-btn");
     imgAction.alt = action;
     imgAction.src = ryuMoveset[action];
